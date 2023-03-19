262e3f1cad31b45e18ff3ebb3ffd5e5988e64bc4
Chas Cates <catescharlton@gmail.com>
2023-03-19 16:49:38 -0500

    2. Transfer Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -2,13 +2,34 @@
 pragma solidity ^0.8.0;
 
 contract DappToken {
-    // Constructor
-    // Set total number of tokens
-    // Read the total number of tokens
+    string public name = "DApp Token";    
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
-    
-    constructor() {
-        totalSupply = 1000000;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value   
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
+    // Transfer Function
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // Exception if account lacks funds
+        require(balanceOf[msg.sender] >= _value);
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        // Transfer Event
+        emit Transfer(msg.sender, _to, _value);
+        // Return Boolean
+        return true;
+    }
 }
